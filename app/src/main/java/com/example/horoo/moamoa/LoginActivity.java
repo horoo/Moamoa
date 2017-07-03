743e9ab6a046fb82a16c65c2c60b4d6da626a71f
eomsanghyun <pnukk12@gmail.com>
2017-07-03 22:27:32 +0900

    login activity

---
+++ app/src/main/java/com/example/horoo/moamoa/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.horoo.moamoa;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.view.*;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText idInput, passwordInput;
+    Button loginBtn, signUpBtn;
+    CheckBox autoLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        idInput = (EditText) findViewById(R.id.idInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        loginBtn = (Button) findViewById((R.id.loginButton));
+        signUpBtn = (Button) findViewById((R.id.signupButton));
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+    }
+
+
+}
